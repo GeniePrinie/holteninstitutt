e50ae8c917fc5b47995536731937535a0ed76ad1
genieprinie <geniexpn@gmail.com>
2024-10-24 19:40:06 +0200

    Update nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "@emotion/react";
+import { useTheme } from "@mui/material/styles";
 import { Box, Container, Link, Typography } from "@mui/material";
 import React from "react";
 import { useLocation } from "react-router-dom";
@@ -7,23 +7,83 @@ const Navbar: React.FC = () => {
   const theme = useTheme();
   const location = useLocation();
 
+  const isActive = (path: string) => location.pathname === path;
+
   return (
-    <>
-      <Container>
-        <Box>
-          <Link href="/" variant="body1">
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "baseline",
+        marginTop: "2rem",
+      }}
+    >
+      <Box>
+        <Link href="/" variant="body1" sx={{ textDecoration: "none" }}>
+          <img
+            src="/images/holtenhorizontal.png"
+            alt="logo"
+            style={{ width: "200px", height: "auto" }}
+          />
+        </Link>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: "3rem",
+        }}
+      >
+        <Box sx={{ width: "2.5rem" }}>
+          <Link
+            href="/"
+            variant="body1"
+            sx={{
+              textDecoration: "none",
+              color: theme.palette.secondary.main,
+              fontWeight: isActive("/") ? "bold" : "normal",
+              "&:hover": {
+                fontWeight: "bold",
+              },
+            }}
+          >
             Hjem
           </Link>
-          <Link href="/fysioterapeuter" variant="body1">
+        </Box>
+        <Box sx={{ width: "7.2rem" }}>
+          <Link
+            href="/fysioterapeuter"
+            variant="body1"
+            sx={{
+              textDecoration: "none",
+              color: theme.palette.secondary.main,
+              fontWeight: isActive("/fysioterapeuter") ? "bold" : "normal",
+              "&:hover": {
+                fontWeight: "bold",
+              },
+            }}
+          >
             Fysioterapeuter
           </Link>
-
-          <Link href="/kontakt" variant="body1">
+        </Box>
+        <Box sx={{ width: "4rem" }}>
+          <Link
+            href="/kontakt"
+            variant="body1"
+            sx={{
+              textDecoration: "none",
+              color: theme.palette.secondary.main,
+              fontWeight: isActive("/kontakt") ? "bold" : "normal",
+              "&:hover": {
+                fontWeight: "bold",
+              },
+            }}
+          >
             Kontakt
           </Link>
         </Box>
-      </Container>
-    </>
+      </Box>
+    </Container>
   );
 };
 
