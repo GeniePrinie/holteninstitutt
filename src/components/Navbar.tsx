aa5f8ae3ef09c7794599d8b225fc7096bdcd78a1
genieprinie <geniexpn@gmail.com>
2024-11-08 10:56:35 +0100

    Update navbar hamburger menu in smaller screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,86 @@
 import { useTheme } from "@mui/material/styles";
-import { Box, Container, Link, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Container,
+  Link,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const theme = useTheme();
   const location = useLocation();
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const isActive = (path: string) => location.pathname === path;
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setDrawerOpen(open);
+    };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= theme.breakpoints.values.md) {
+        setDrawerOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [theme.breakpoints.values.md]);
+
+  const menuItems = [
+    { text: "Hjem", path: "/" },
+    { text: "Fysioterapeuter", path: "/fysioterapeuter" },
+    { text: "Kontakt", path: "/kontakt" },
+  ];
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {menuItems.map((item) => (
+          <ListItem key={item.text} component="a" href={item.path}>
+            <ListItemText
+              primary={
+                <Typography
+                  sx={{
+                    color: theme.palette.primary.main,
+                    fontWeight: "normal",
+                    "&:hover": {
+                      color: theme.palette.secondary.main,
+                      fontWeight: 700,
+                    },
+                  }}
+                >
+                  {item.text}
+                </Typography>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <Container
@@ -30,59 +103,57 @@ const Navbar: React.FC = () => {
 
       <Box
         sx={{
-          display: "flex",
+          display: { xs: "none", md: "flex" },
           gap: "3rem",
         }}
       >
-        <Box sx={{ width: "2.5rem" }}>
-          <Link
-            href="/"
-            variant="body1"
+        {menuItems.map((item) => (
+          <Box
+            key={item.text}
             sx={{
-              textDecoration: "none",
-              color: theme.palette.secondary.main,
-              fontWeight: isActive("/") ? "bold" : "normal",
-              "&:hover": {
-                fontWeight: "bold",
-              },
+              width: item.text === "Fysioterapeuter" ? "7.2rem" : "100%",
             }}
           >
-            Hjem
-          </Link>
-        </Box>
-        <Box sx={{ width: "7.2rem" }}>
-          <Link
-            href="/fysioterapeuter"
-            variant="body1"
-            sx={{
-              textDecoration: "none",
-              color: theme.palette.secondary.main,
-              fontWeight: isActive("/fysioterapeuter") ? "bold" : "normal",
-              "&:hover": {
-                fontWeight: "bold",
-              },
-            }}
-          >
-            Fysioterapeuter
-          </Link>
-        </Box>
-        <Box sx={{ width: "4rem" }}>
-          <Link
-            href="/kontakt"
-            variant="body1"
-            sx={{
-              textDecoration: "none",
-              color: theme.palette.secondary.main,
-              fontWeight: isActive("/kontakt") ? "bold" : "normal",
-              "&:hover": {
-                fontWeight: "bold",
-              },
-            }}
-          >
-            Kontakt
-          </Link>
-        </Box>
+            <Link
+              href={item.path}
+              variant="body1"
+              sx={{
+                textDecoration: "none",
+                color: theme.palette.secondary.main,
+                fontWeight: isActive(item.path) ? "bold" : "normal",
+                "&:hover": {
+                  fontWeight: "bold",
+                },
+              }}
+            >
+              {item.text}
+            </Link>
+          </Box>
+        ))}
       </Box>
+
+      <IconButton
+        edge="end"
+        color="inherit"
+        aria-label="menu"
+        sx={{
+          display: { xs: "block", md: "none" },
+          color: theme.palette.secondary.main,
+          width: "3rem",
+          height: "3rem",
+          borderRadius: "50%",
+          "&:hover": {
+            backgroundColor: theme.palette.tertiary.main,
+          },
+        }}
+        onClick={toggleDrawer(true)}
+      >
+        <MenuIcon sx={{ fontSize: "2rem" }} />
+      </IconButton>
+
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        {list()}
+      </Drawer>
     </Container>
   );
 };
